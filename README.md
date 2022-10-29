5c19a693b07ebe8a54ea3cc1806932139751d102
Hulend <59649037+Hulend@users.noreply.github.com>
2022-10-29 02:51:22 -0700

    Explore Part 2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Lab 5 - Starter
 Name: Michael Tedjo
+https://github.com/Hulend/introduction-to-github
+https://hulend.github.io/Lab5_Starter/explore.html
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,24 +4,23 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   const speech = window.speechSynthesis;
-  const voices = speech.getVoices();
-  const select = document.getElementById("voice-select");
-  voices.forEach((text,index) => {
-    let opt = document.createElement("option");
-    opt.value = index;
-    opt.textContent = text;
-    select.appendChild(opt);
-  });
-  
-  const play = document.querySelector("button");
-  const input = document.getElementById("text-to-speak");
-  const face = document.querySelector("img");
-  play.addEventListener('click', function() {
-    const utter = new SpeechSynthesisUtterance(input.value)
-    if(!isNaN(select.value)) { utter.voice = voices[select.value] }
-    else { return; }
-    face.src = "assets/images/smiling-open.png";
-    speech.speak(utter);
-    face.src = "assets/images/smiling.png";
+  speech.addEventListener("voiceschanged", () => {
+    const voices = speech.getVoices();
+    const select = document.getElementById("voice-select");
+    for(let index = 0; index < voices.length; index++) {
+      select.options[index] = new Option(voices[index].name, index);
+    }
+    
+    const play = document.querySelector("button");
+    const input = document.getElementById("text-to-speak");
+    const face = document.querySelector("img");
+    play.addEventListener('click', function() {
+      const utter = new SpeechSynthesisUtterance(input.value)
+      if(!isNaN(select.value)) { utter.voice = voices[select.value] }
+      else { return; }
+      face.src = "assets/images/smiling-open.png";
+      speech.speak(utter);
+      face.src = "assets/images/smiling.png";
+    });
   })
 }
